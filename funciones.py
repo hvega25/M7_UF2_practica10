af9872a4a0c6a2e75cc567b2131b99c6eea1cd87
hvega25 <hvega@jaumebalmes.com>
2023-01-19 18:55:26 +0100

    practica hecha

--- funciones.py
+++ funciones.py
@@ -1,19 +1,11 @@
-import pandas as pd
+import pandas as pd #importa la libreria para leer el csv
 
-def mostrarXid(ID):
+def mostrarClockSpeed(ID):#funcion que retorna la velocidad de procesador
 
-    df = pd.read_csv('test.csv')
-    df_mask = df['id'] == ID
-    filtered_df = df[df_mask]
-    print(filtered_df['clock_speed'])
-
-def mostrarPixe(ID):
-
-    df = pd.read_csv('test.csv')
-    df_mask = df['id'] == ID
-    filtered_df = df[df_mask]
-    return filtered_df['px_width']
-
+    df = pd.read_csv('test.csv') #lee un archivo csv y lo guarda en una variable
+    df_mask = df['id'] == ID #compara si el valor de la columna id de la tabla es igual al valor de parametro y lo guarda
+    filtered_df = df[df_mask] # guarda el valor encontrado en una variable
+    return filtered_df['clock_speed'] #retorno el valor
 
 def mostrarPixe(ID):
 
@@ -21,7 +13,6 @@ def mostrarPixe(ID):
     df_mask = df['id'] == ID
     filtered_df = df[df_mask]
     return filtered_df['px_width']
-
 
 
 def mostrarBattery(ID):
--- main.py
+++ main.py
@@ -1,46 +1,28 @@
+import matplotlib.pyplot as plt #libreria para imprimir un grafico
+import funciones #importa la hoja de funciones
 
-import matplotlib.pyplot as plt
-import funciones
 
+def graficar_bateria(): #funcion que grafica
+    contador =0 #primer contador que se usa para invocar a la funcion
+    contador1=0 #segundo contador que se usa para la grafica
+    list = [3, 13, 34, 56, 70, 85, 110, 120, 210, 400]  # lista de los valores a buscar
+    imprimir = [] #arreglo donde se guardan las valores que retorna
 
-def graficar_bateria():
-    contador =0
-    contador1=0
-    list = [3, 13, 34, 56, 70, 85, 110, 120, 210, 400]
-    imprimir = []
-
-    while contador < len(list):
+    while contador < len(list): #primer bucle que guarda en una variable lo que se ha invocado en las funciones
         imprimir.append(funciones.mostrarBattery(list[contador]))
         contador = contador + 1
 
 
     while contador1 < len(list):
         plt.bar(contador1 , imprimir[contador1])
+        contador1 = contador1 + 1
 
     plt.xlabel('Indice')
-    plt.ylabel('mhz')
+    plt.ylabel('mAh')
     plt.show()
 
 
-def graficar_clockSpeed():
-    contador =0
-    contador1=0
-    list = [3, 13, 34, 56, 70, 85, 110, 120, 210, 400]
-    imprimir = []
-
-    while contador < len(list):
-        imprimir.append(funciones.mostrarXid(list[contador]))
-        contador = contador + 1
-
-
-    while contador1 < len(list):
-        plt.bar(contador1 , imprimir[contador1])
-
-    plt.xlabel('Indice')
-    plt.ylabel('Clock speed')
-    plt.show()
-
-def graficar_megaPixel():
+def graficar_MEGAPIXEL():
     contador =0
     contador1=0
     list = [3, 13, 34, 56, 70, 85, 110, 120, 210, 400]
@@ -53,14 +35,33 @@ def graficar_megaPixel():
 
     while contador1 < len(list):
         plt.bar(contador1 , imprimir[contador1])
+        contador1 = contador1 + 1
 
     plt.xlabel('Indice')
-    plt.ylabel('Clock speed')
+    plt.ylabel('MEGAPIXEL')
     plt.show()
 
 
+def graficar_MHZ():
+    contador =0
+    contador1=0
+    list = [3, 13, 34, 56, 70, 85, 110, 120, 210, 400]
+    imprimir = []
+
+    while contador < len(list):
+        imprimir.append(funciones.mostrarClockSpeed(list[contador]))
+        contador = contador + 1
+
+
+    while contador1 < len(list):
+        plt.bar(contador1 , imprimir[contador1])
+        contador1 = contador1 + 1
+
+    plt.xlabel('Indice')
+    plt.ylabel('mhz')
+    plt.show()
 
 
 graficar_bateria()
-graficar_clockSpeed()
-graficar_megaPixel()
+graficar_MEGAPIXEL()
+graficar_MHZ()
