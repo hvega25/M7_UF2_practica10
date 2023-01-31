8303e07454fa629ea47e0c94fd5e8ce40780ce46
hvega25 <hvega@jaumebalmes.com>
2023-01-31 20:46:43 +0100

    Practica terminada pero sin comentarios

--- funciones.py
+++ funciones.py
@@ -3,17 +3,21 @@ import pandas as pd
 def poblacion():
     df = pd.read_csv('List of cities proper by population density11.csv')
     ciudades = pd.DataFrame(df, columns=["City", "Population"])
-    print(ciudades.head(10))
+    retorno = ciudades.head(10)
+    return retorno
+
 
 
 
 def KM2():
     df = pd.read_csv('List of cities proper by population density11.csv')
-    ciudades = pd.DataFrame(df, columns=["City", "Area KM2"])
-    print( ciudades.head(10))
+    ciudades = pd.DataFrame(df, columns=["City", "Density KM2"])
+    retorno = ciudades.head(10)
+    return retorno
 
 
 def M2():
     df = pd.read_csv('List of cities proper by population density11.csv')
-    ciudades = pd.DataFrame(df, columns=["City", "Area   M2"])
-    print( ciudades.head(10))
+    ciudades = pd.DataFrame(df, columns=["City", "Density  M2"])
+    retorno = ciudades.head(10)
+    return retorno
--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import funciones
 import matplotlib.pyplot as plt
 
-def imprimir():
-    funciones.poblacion()
-    funciones.M2()
-    funciones.KM2()
+
+def imprimir_poblacion():
+    retorno = funciones.poblacion()
+    fig, ax = plt.subplots(figsize=(8, 5))
+    ax.pie(retorno['Population'].astype(str).str.replace(',', '').astype(float),  labels=retorno['City'], autopct='%1.1f%%', startangle=90)
+    plt.title('Poblacion')
+    plt.legend()
+    plt.show()
+
+def imprimir_poblacion_KM2():
+    retorno = funciones.KM2()
+    fig, ax = plt.subplots(figsize=(8, 5))
+    ax.pie(retorno['Density KM2'].astype(str).str.replace(',', '').astype(float), labels=retorno['City'], autopct='%1.1f%%', startangle=90)
+    plt.title('Densidad kM2')
+    plt.legend()
+    plt.show()
+
+def imprimir_poblacion_M2():
+    retorno = funciones.M2()
+    fig, ax = plt.subplots(figsize=(8, 5))
+    ax.pie(retorno['Density  M2'].astype(str).str.replace(',', '').astype(float), labels=retorno['City'], autopct='%1.1f%%', startangle=90)
+    plt.title('Densidad M2')
+    plt.legend()
+    plt.show()
 
 
-
-
-
-imprimir()
+imprimir_poblacion()
+imprimir_poblacion_KM2()
+imprimir_poblacion_M2()
